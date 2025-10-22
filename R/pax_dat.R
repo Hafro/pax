3df27977778dc430877e420f11e5643095b1c403
Jamie Lentin <jm@lentin.co.uk>
2025-10-22 16:07:51 +0000

    pax_dat: Make strata/reitmapping a pax_dat
    They're both ~internal generated data files, so should be pax_dat_.
    
    The logic of reitmapping should also be in hafropax, at least for now.

--- R/pax_dat.R
+++ R/pax_dat.R
@@ -1,10 +1,8 @@
 pax_dat_noaa_bathymetry <- function(pcon) UseMethod("pax_dat_noaa_bathymetry")
 pax_dat_lw_coeffs <- function(pcon) UseMethod("pax_dat_lw_coeffs")
 pax_dat_reitmapping <- function(pcon) UseMethod("pax_dat_reitmapping")
-
-# Was: tidypax::add_regions (data)
-pax_dat_reitmapping.pax <- function(pcon) {
-  # TODO: reitmapping *data* is ~iceland-specific, but not hafropax-specific
-  # TODO: Generation: https://gitlab.hafogvatn.is/dag/00-setup/-/blob/master/SI_db_setup/01b-dbsetup.R#L440
-  mar::tbl_mar(pcon$dbcon, 'ops$bthe."reitmapping"') |> pax_tbl(pcon = pcon)
+pax_dat_strata_stations <- function(pcon) UseMethod("pax_dat_strata_stations")
+pax_dat_strata_attributes <- function(pcon) {
+  UseMethod("pax_dat_strata_attributes")
 }
+pax_dat_strata_areas <- function(pcon) UseMethod("pax_dat_strata_areas")
--- R/pax_strata.R
+++
@@ -1,5 +0,0 @@
-pax_si_strata_stations <- function(pcon) UseMethod("si_strata_stations")
-pax_si_strata_attributes <- function(pcon) UseMethod("si_strata_attributes")
-pax_si_strata_areas <- function(pcon) UseMethod("si_strata_areas")
-
-# TODO: Can we make duckpax versions of these? Does that make sense?
