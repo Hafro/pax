9c994d820b1bb0e5a9257eabce2e677cc515a837
Jamie Lentin <jm@lentin.co.uk>
2025-12-23 16:36:18 +0000

    inttest/test-02_had: Test ALKs as much as we can
    Proper strata are stopping us getting over the finish line, but
    everything else seems to be working.

--- inttest/test-02_had.R
+++ inttest/test-02_had.R
@@ -28,11 +28,399 @@ if (!file.exists("/tmp/camel.duckdb")) {
   pax_import(pcon, do.call(pax_mar_measurement, import_defs))
   pax_import(pcon, do.call(pax_mar_catch, import_defs))
   pax_import(pcon, do.call(pax_mar_sampling, import_defs))
+  pax_import(pcon, pax_mar_aldist(mar, species = import_defs$species))
   pax_import(pcon, pax_mar_ldist(mar, species = import_defs$species))
   pax_import(pcon, pax_mar_lw_coeffs(mar, species = import_defs$species))
 } else {
   pcon <- pax::pax_connect("/tmp/camel.duckdb")
 }
+
+ok_group("input_data.R:Generate the ALK from the survey", {
+  tidypax_igfs_alk <-
+    tidypax::si_stations(mar) |>
+    dplyr::filter(sampling_type %in% 30, nvl(tow_number, 0) %in% 0:35) |>
+    tidypax::si_make_alk(
+      tgroup = NULL,
+      regions = list(
+        S = c(101, 107, 106, 108, 109, 114),
+        N = c(102, 103, 104, 105, 111, 113)
+      ),
+      gear_group = NULL,
+      aldist = function(src) {
+        mar::les_aldur(mar) |>
+          dplyr::filter(tegund_nr == 2) |>
+          dplyr::select(
+            sample_id = synis_id,
+            species = tegund_nr,
+            length = lengd,
+            age = aldur,
+            count = fjoldi
+          ) |>
+          dplyr::group_by(sample_id, species, length, age) |>
+          dplyr::summarize(count = sum(count))
+      }
+    ) |>
+    dplyr::filter(species == local(species_code))
+
+  newpax_igfs_alk <-
+    dplyr::tbl(pcon, "si") |>
+    dplyr::filter(sampling_type %in% 30, coalesce(tow_number, 0) %in% 0:35) |>
+    pax_si_make_alk(
+      tgroup = NULL,
+      regions = list(
+        S = c(101, 107, 106, 108, 109, 114),
+        N = c(102, 103, 104, 105, 111, 113)
+      ),
+      gear_group = NULL
+    ) |>
+    dplyr::filter(ygroup == 1990) |>
+    dplyr::arrange(ygroup, gear_name, region, species, tgroup, lgroup, age) |>
+    dplyr::collect()
+
+  ok(
+    ut_cmp_equal(
+      tidypax_igfs_alk |>
+        dplyr::filter(ygroup == 1990) |>
+        dplyr::arrange(
+          ygroup,
+          gear_name,
+          region,
+          species,
+          tgroup,
+          lgroup,
+          age,
+          agep
+        ) |>
+        dplyr::select(
+          ygroup,
+          gear_name,
+          region,
+          species,
+          tgroup,
+          lgroup,
+          age,
+          agep
+        ) |>
+        as.data.frame(),
+      newpax_igfs_alk |>
+        dplyr::filter(ygroup == 1990) |>
+        dplyr::arrange(
+          ygroup,
+          gear_name,
+          region,
+          species,
+          tgroup,
+          lgroup,
+          age,
+          agep
+        ) |>
+        dplyr::select(
+          ygroup,
+          gear_name,
+          region,
+          species,
+          tgroup,
+          lgroup,
+          age,
+          agep
+        ) |>
+        as.data.frame(),
+      tolerance = 1e-6
+    ),
+    "Data frames match in 1990"
+  )
+
+  ok(
+    ut_cmp_equal(
+      mar::les_lengd(mar) |>
+        mar::skala_med_taldir() |>
+        dplyr::select(
+          sample_id = synis_id,
+          species = tegund_nr,
+          length = lengd,
+          sex = kyn_nr,
+          count = fjoldi
+        ) |>
+        dplyr::mutate(sample_id = as.numeric(sample_id)) |> # NB: sample_id is character, we need integer to sort it
+        dplyr::filter(species == 2) |> # NB: Our ldist is broken down by species, we should do the same here
+        dplyr::group_by(sample_id, species, length, sex) |>
+        dplyr::summarize(count = sum(count)) |> # NB: les_lengd doesn't group
+        dplyr::arrange(sample_id, species, length, sex) |>
+        as.data.frame(),
+      dplyr::tbl(pcon, "ldist") |>
+        dplyr::mutate(sample_id = as.numeric(sample_id)) |> # NB: sample_id is character, we need integer to sort it
+        dplyr::arrange(sample_id, species, length, sex) |>
+        as.data.frame(),
+      end = NULL
+    ),
+    "ldist matches"
+  )
+  ok(
+    ut_cmp_equal(
+      mar::les_aldur(mar) |>
+        dplyr::select(
+          sample_id = synis_id,
+          species = tegund_nr,
+          length = lengd,
+          age = aldur,
+          count = fjoldi
+        ) |>
+        dplyr::mutate(sample_id = as.numeric(sample_id)) |> # NB: sample_id is character, we need integer to sort it
+        dplyr::filter(species == 2) |> # NB: Our ldist is broken down by species, we should do the same here
+        dplyr::group_by(sample_id, species, length, age) |>
+        dplyr::summarize(count = sum(count)) |> # NB: les_lengd doesn't group
+        dplyr::arrange(sample_id, species, length, age) |>
+        as.data.frame(),
+      dplyr::tbl(pcon, "aldist") |>
+        dplyr::mutate(sample_id = as.numeric(sample_id)) |> # NB: sample_id is character, we need integer to sort it
+        dplyr::arrange(sample_id, species, length, age) |>
+        as.data.frame(),
+      end = NULL
+    ),
+    "aldist matches"
+  )
+
+  tidypax_lw_dat <-
+    mar::les_stod(mar) |>
+    dplyr::left_join(mar::les_syni(mar)) |>
+    dplyr::left_join(mar::les_aldur(mar)) |>
+    dplyr::filter(synaflokkur_nr == 30, tegund_nr == local(species_code)) |>
+    dplyr::select(species = tegund_nr, length = lengd, weight = thyngd) |>
+    dplyr::filter(!is.na(length), weight > 0) |>
+    dplyr::collect(n = Inf)
+  lw_pred <-
+    tibble::tibble(species = species_code, length = 1:150) |>
+    modelr::add_predictions(
+      gam::gam(
+        weight ~ gam::s(log(length), df = 8),
+        family = Gamma(link = log),
+        data = tidypax_lw_dat
+      ),
+      var = 'weight'
+    ) |>
+    dplyr::mutate(weight = as.numeric(exp(weight)))
+
+  tidypax_igfs_by_length <-
+    si_stations(mar) |>
+    dplyr::filter(sampling_type %in% 30, nvl(tow_number, 0) %in% 0:35) |>
+    si_by_length(species = species_code, ldist = function(src) {
+      mar::les_lengd(mar) |>
+        mar::skala_med_taldir() |>
+        dplyr::select(
+          sample_id = synis_id,
+          species = tegund_nr,
+          length = lengd,
+          sex = kyn_nr,
+          count = fjoldi
+        ) |>
+        dplyr::filter(species == 2) |> # NB: newpax's ldist is broken down by species, we should do the same here
+        dplyr::group_by(sample_id, species, length, sex) |>
+        dplyr::summarize(count = sum(count)) |> # NB: les_lengd doesn't group
+        dplyr::left_join(
+          dbplyr::copy_inline(mar, lw_pred),
+          by = c("species", 'length')
+        )
+    }) |>
+    identity()
+  newpax_igfs_by_length <-
+    dplyr::tbl(pcon, "si") |>
+    dplyr::filter(sampling_type %in% 30, coalesce(tow_number, 0) %in% 0:35) |>
+    pax_si_by_length(
+      ldist = dplyr::tbl(pcon, "ldist") |>
+        dplyr::left_join(
+          pax_temptbl(pcon, lw_pred),
+          by = c("species", 'length')
+        )
+    ) |>
+    identity()
+  ok(
+    ut_cmp_equal(
+      tidypax_igfs_by_length |>
+        dplyr::filter(year == 1990) |>
+        dplyr::arrange(year, month, station, gear, species, length, sex) |>
+        dplyr::ungroup() |>
+        dplyr::select(
+          year,
+          month,
+          station,
+          begin_lat,
+          begin_lon,
+          tow_length,
+          gear,
+          species,
+          length,
+          sex,
+          N,
+          B
+        ) |>
+        as.data.frame(),
+      newpax_igfs_by_length |>
+        dplyr::rename(gear = mfdb_gear_code) |>
+        dplyr::filter(year == 1990) |>
+        dplyr::arrange(year, month, station, gear, species, length, sex) |>
+        dplyr::ungroup() |>
+        dplyr::select(
+          year,
+          month,
+          station,
+          begin_lat,
+          begin_lon,
+          tow_length,
+          gear,
+          species,
+          length,
+          sex,
+          N,
+          B
+        ) |>
+        as.data.frame(),
+      end = NULL
+    ),
+    "tidypax_igfs_by_length: Match for 1990"
+  )
+
+  tidypax_igfs_at_age <-
+    si_stations(mar) |>
+    dplyr::filter(sampling_type %in% 30, nvl(tow_number, 0) %in% 0:35) |>
+    dplyr::filter(
+      year >= local(import_defs$year_start),
+      year <= local(import_defs$year_end)
+    ) |>
+    si_by_length(species = species_code, ldist = function(src) {
+      mar::les_lengd(mar) |>
+        mar::skala_med_taldir() |>
+        dplyr::select(
+          sample_id = synis_id,
+          species = tegund_nr,
+          length = lengd,
+          sex = kyn_nr,
+          count = fjoldi
+        ) |>
+        dplyr::filter(species == 2) |> # NB: newpax's ldist is broken down by species, we should do the same here
+        dplyr::group_by(sample_id, species, length, sex) |>
+        dplyr::summarize(count = sum(count)) |> # NB: les_lengd doesn't group
+        dplyr::left_join(
+          dbplyr::copy_inline(mar, lw_pred),
+          by = c("species", 'length')
+        )
+    }) |>
+    si_by_age(
+      post_scaling = function(x, ...) {
+        x
+      },
+      tgroup = NULL,
+      regions = list(
+        S = c(101, 107, 106, 108, 109, 114),
+        N = c(102, 103, 104, 105, 111, 113)
+      ),
+      gear_group = NULL,
+      alk = tidypax_igfs_alk
+    ) |>
+    dplyr::filter(stratification == 'new_strata') # NB: Was old_strata, but haven't loaded that
+
+  newpax_igfs_at_age <-
+    dplyr::tbl(pcon, "si") |>
+    dplyr::filter(sampling_type %in% 30, coalesce(tow_number, 0) %in% 0:35) |>
+    pax_si_by_length(
+      ldist = dplyr::tbl(pcon, "ldist") |>
+        dplyr::left_join(
+          pax_temptbl(pcon, lw_pred),
+          by = c("species", 'length')
+        )
+    ) |>
+    pax_add_strata("new_strata") |>
+    pax_si_by_age(
+      tgroup = NULL,
+      regions = list(
+        S = c(101, 107, 106, 108, 109, 114),
+        N = c(102, 103, 104, 105, 111, 113)
+      ),
+      gear_group = NULL,
+      alk = newpax_igfs_alk
+    )
+
+  if (FALSE) { # TODO: Incorrect strata is breaking scaling
+  tidypax_igfs_at_age |>
+    dplyr::filter(sample_id == 44490, age == 2) |>
+    dplyr::arrange(length) |>
+    dplyr::select(station, length, stratum, age, agep)
+  newpax_igfs_at_age |>
+    dplyr::filter(sample_id == 44490, age == 2) |>
+    dplyr::arrange(length) |>
+    dplyr::select(
+      station,
+      length,
+      begin_lat,
+      begin_lon,
+      end_lat,
+      end_lon,
+      stratum,
+      age,
+      agep
+    )
+
+  ok(
+    ut_cmp_equal(
+      tidypax_igfs_at_age |>
+        dplyr::filter(year == 1990, station == 3661273) |>
+        dplyr::arrange(
+          ygroup,
+          gear_name,
+          region,
+          species,
+          tgroup,
+          lgroup,
+          age
+        ) |>
+        dplyr::ungroup() |>
+        dplyr::select(
+          ygroup,
+          gear_name,
+          region,
+          species,
+          tgroup,
+          lgroup,
+          age,
+          agep,
+          N,
+          adj_N,
+          adj_B
+        ) |>
+        as.data.frame(),
+      newpax_igfs_at_age |>
+        dplyr::rename(gear = mfdb_gear_code) |>
+        dplyr::filter(year == 1990, station == 3661273) |>
+        dplyr::arrange(
+          ygroup,
+          gear_name,
+          region,
+          species,
+          tgroup,
+          lgroup,
+          age
+        ) |>
+        dplyr::ungroup() |>
+        dplyr::select(
+          ygroup,
+          gear_name,
+          region,
+          species,
+          tgroup,
+          lgroup,
+          age,
+          agep,
+          N,
+          adj_N,
+          adj_B
+        ) |>
+        # TODO: Why do we still have an h3_cell column?
+        as.data.frame(),
+      end = NULL
+    ),
+    "newpax_igfs_at_age: Matches tidypax at 1990"
+  )
+  }  # TODO: End
+})
 
 ok_group("R/R/06-surveyplots.R:survey index by area", {
   # Do the si by_strata query, and extract the station/stratum mapping from it
