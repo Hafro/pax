f03df20fb12f162c4b37e65dd424cd105d7afae6
Jamie Lentin <jm@lentin.co.uk>
2026-01-19 18:29:30 +0000

    pax_strata: Rework strata import
    The strafications contain multiple overlapping schemes, for the
    shapefiles to be useful these need to be split out.
    
    Separate "new_strata" into spring & summer variants, keep everything in
    "old_strata" that doesn't clash with existing entries in
    strata_stations.
    
    Break up sets of stratum points into substratum where appropriate, so
    we end up with multipolygons of distinct shapes rather than splurging
    them together.
    
    Finally, remove some points that result in misshapen corners.
    
    References: https://github.com/Hafro/haftaf/issues/12

--- R/pax_strata.R
+++ R/pax_strata.R
@@ -1,24 +1,100 @@
 # Routine used to extract strata from mar
-export_mar_strata <- function(strata_name) {
-  shp_path <- paste0("pax/inst/extdata/strata_", strata_name, ".shp")
+export_mar_strata <- function(mar, strata_name, substratification = "default") {
+  shp_path <- paste0(
+    "pax/inst/extdata/strata_",
+    strata_name,
+    (if (substratification == "default") "" else paste0("_", substratification)),
+    ".shp")
   if (file.exists(shp_path)) {
     unlink(gsub(".shp$", ".*", shp_path))
   }
+
+  # Pregenerate substring to match substratification
+  substratification_match <- paste0(":", substratification, ":")
+
   # NB: area is (in theory) the area of the stratum. "rall_area" ~ "surveyable_area",
   #     i.e. the area in the stratum deep enough for the survey to reach.
   mar::tbl_mar(mar, 'ops$bthe."strata_areas"') |>
     dplyr::filter(stratification == local(strata_name)) |>
-    dplyr::select(-stratification) |>
     dplyr::filter(!is.na(lat), !is.na(lon)) |>
-    dplyr::arrange(stratum, order) |>
     dplyr::collect() |>
-    dplyr::mutate(stratum = as.integer(stratum), order = as.integer(order)) |>
-    dplyr::group_by(stratum) |>
+    dplyr::mutate(stratum = as.integer(stratum), order = as.integer(order)) |> # NB: This won't work in dbplyr
+
+    # Assign substratification, either throwing away unused stratum or breaking up into separate non-overlapping sets
+    dplyr::mutate(substratification = dplyr::case_when(
+      # Remove overlapping strata not mentioned in strata_stations
+      # Will likely be sets of equivalent stratum here too, but not searched for
+      stratification == "old_strata" & stratum %in% c(24:25, 30:32, 37L, 52L, 59L, 69L, 70:78, 86L, 94L) ~ ":unused:",
+      # Sets of equivalent stratum in new_strata, any substratification needs to choose one:
+      # 45 <--> 22, 23, 28
+      # 42, 43, 44 <--> 4, 7, 9, 12
+      # 13 <--> 14, 15 <--> 15, 36, 37 <--> 36, 38
+      # 21, 28 <--> 45
+      # 18 <--> 29, 30
+      stratification == "new_strata" & stratum %in% c(42, 43, 44, 15, 37) ~ ":spring:",
+      stratification == "new_strata" & stratum %in% c(4, 7, 9, 12, 38) ~ ":autumn:",
+      # No substratification that uses these
+      stratification == "new_strata" & stratum %in% c(22, 23, 28, 13, 14, 21, 28, 29, 30) ~ ":unused:",
+      # Assume every other stratum is common
+      stratification == "new_strata" ~ ":spring:autumn:",
+      TRUE ~ ":default:",
+    )) |>
+
+    # Assign substratum, either breaking up multiple polygons or binning nonsense points
+    dplyr::mutate(substratum = dplyr::case_when(
+      # Remove nonsense points
+      stratification == "old_strata" & stratum == 67 & order > 11775 ~ NA,
+      # Break up multipolygons
+      stratification == "old_strata" & stratum == 54 & order < 9329 ~ 1,
+      stratification == "old_strata" & stratum == 54 & order < 9368 ~ 2,
+      stratification == "old_strata" & stratum == 54 ~ 3,
+      # Break up multipolygons
+      stratification == "old_strata" & stratum == 58 & order < 9810 ~ 1,
+      stratification == "old_strata" & stratum == 58 & order < 9881 ~ 2,
+      stratification == "old_strata" & stratum == 58 ~ 3,
+      # Break up multipolygons
+      stratification == "old_strata" & stratum == 89 & order < 14242 ~ 1,
+      stratification == "old_strata" & stratum == 89 & order < 14257 ~ 2,
+      stratification == "old_strata" & stratum == 89 & order < 14285 ~ 3,
+      stratification == "old_strata" & stratum == 89 & order < 14291 ~ 4,
+      stratification == "old_strata" & stratum == 89 & order < 14303 ~ 5,
+      stratification == "old_strata" & stratum == 89 & order < 14337 ~ 6,
+      stratification == "old_strata" & stratum == 89 ~ 7,
+      # Break up multipolygons
+      stratification == "new_strata" & stratum == 41 & order %in% 65697:65736 ~ 1,
+      stratification == "new_strata" & stratum == 41 & order %in% 65737:65762 ~ 2,
+      stratification == "new_strata" & stratum == 41 & order %in% 65764:65811 ~ 3,
+      stratification == "new_strata" & stratum == 41 & order %in% 65813:65837 ~ 4,
+      stratification == "new_strata" & stratum == 41 & order %in% 65838:65856 ~ 5,
+      # Remove nodes attempting to join the polygons
+      stratification == "new_strata" & stratum == 41 ~ NA,
+      # Remove lump
+      stratification == "new_strata" & stratum == 9 & (order %in% 7594:7623) ~ NA,
+      # Remove part of stratum 9
+      stratification == "new_strata" & stratum == 12 & (order %in% 13379:13400) ~ NA,
+      # Remove duplicated corner
+      stratification == "new_strata" & stratum == 19 & (order %in% 29957:29962) ~ NA,
+      stratification == "new_strata" & stratum == 19 & order == 29092 ~ NA,
+      # Remove overlap corner (should be replacing it with a point further northwest)
+      stratification == "new_strata" & stratum == 45 & (order %in% 71842) ~ NA,
+      TRUE ~ 1,
+    )) |>
+
+    # Remove rows filtered by the above
+    dplyr::filter(is.finite(substratum)) |>
+    dplyr::filter(grepl(local(substratification_match), substratification, fixed = TRUE)) |>
+    dplyr::select(-stratification, -substratification) |>
+
+    # Convert data.frame into geometry of points
     sf::st_as_sf(coords = c("lon", "lat"), crs = 4326) |>
-    dplyr::group_by(stratum) |>
-    dplyr::arrange(stratum, order) |>
+    # Combine points into substratum polygon
+    dplyr::arrange(stratum, substratum, order) |>
+    dplyr::group_by(stratum, substratum) |>
     dplyr::summarise(geometry = sf::st_combine(geometry)) |>
     sf::st_cast("POLYGON") |>
+    # Combine substratum polygon to stratum multipolygon
+    dplyr::group_by(stratum) |>
+    dplyr::summarise(geometry = sf::st_combine(geometry)) |>
     dplyr::left_join(
       mar::tbl_mar(mar, 'biota.strata_attributes') |>
         dplyr::filter(stratification == local(strata_name)) |>
@@ -29,9 +105,16 @@ export_mar_strata <- function(strata_name) {
     ) |>
     sf::st_make_valid() |>
     sf::st_write(shp_path)
-  print(sf::st_is_valid(sf::st_read(shp_path), reason = TRUE))
+  out <- sf::st_read(shp_path)
+  print(sf::st_is_valid(out, reason = TRUE))
+  invisible(out)
 }
-# for (s in c("old_strata", "new_strata", "ghl_strata")) export_mar_strata(s)
+if (FALSE) {
+  pax:::export_mar_strata(mar, "old_strata")
+  pax:::export_mar_strata(mar, "new_strata", "spring")
+  pax:::export_mar_strata(mar, "new_strata", "autumn")
+  pax:::export_mar_strata(mar, "ghl_strata")
+}
 
 pax_def_crs <- function() {
   return(sf::st_crs(4326))
