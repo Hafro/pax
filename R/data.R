17d2127c2efa49283e5665c1d75f21d97d61ae61
Jamie Lentin <jm@lentin.co.uk>
2026-03-18 17:40:41 +0000

    man: Documentation for data objects

---
+++ R/data.R
@@ -0,0 +1,51 @@
+#' Gridcell / division / subdivision mapping
+#'
+#' @name gridcell
+#' @docType data
+#' @keywords data
+NULL
+
+#' Lookup table of MFDB gear code descriptions
+#'
+#' @name mfdb_gear_code_desc
+#' @docType data
+#' @keywords data
+NULL
+
+#' Cached ocean bathymetry for default area bounds
+#'
+#' @name raw_ocean_depth_defbounds
+#' @docType data
+#' @keywords data
+NULL
+
+#' Lookup table of sampling type descriptions
+#'
+#' @name sampling_type_desc
+#' @docType data
+#' @keywords data
+NULL
+
+#' Station tables from the SMB survey handbook
+#'
+#' @name stationlist_hafro_smb
+#' @docType data
+#' @keywords data
+#' @references \url{https://www.hafogvatn.is/static/research/files/rallhandbok_2025-enska-1.pdf}
+NULL
+
+#' Station tables from the SMH survey handbook - deep survey
+#'
+#' @name stationlist_hafro_smh_deep
+#' @docType data
+#' @keywords data
+#' @references \url{https://www.hafogvatn.is/static/research/files/smh_manual_2025.pdf}
+NULL
+
+#'Station tables from the SMH survey handbook - shelf survey
+#'
+#' @name stationlist_hafro_smh_shelf
+#' @docType data
+#' @keywords data
+#' @references \url{https://www.hafogvatn.is/static/research/files/smh_manual_2025.pdf}
+NULL
